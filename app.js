257dddf7accbb3bfff7b7fe45845ea5c7aff2546
Sam Atkins <atkinssj@gmail.com>
2025-04-25 11:29:02 +0100

    Add crude exception handling
    Rather than crashing on error, log it and try to report a 500 error.

--- app.js
+++ app.js
@@ -13,174 +13,182 @@ const PORT = process.env.PORT || 3000;
  * Parse request body and verifies incoming requests using discord-interactions package
  */
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
-  // Interaction id, type and data
-  const { id, type, data } = req.body;
+  try {
+    // Interaction id, type and data
+    const { id, type, data } = req.body;
 
-  /**
-   * Handle verification requests
-   */
-  if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
-  }
+    /**
+     * Handle verification requests
+     */
+    if (type === InteractionType.PING) {
+      return res.send({ type: InteractionResponseType.PONG });
+    }
 
-  /**
-   * Handle slash command requests
-   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
-   */
-  if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
+    /**
+     * Handle slash command requests
+     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+     */
+    if (type === InteractionType.APPLICATION_COMMAND) {
+      const { name } = data;
 
-    async function sendLoadingMessage(text) {
-      return res.send({
-        type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: text,
+      async function sendLoadingMessage(text) {
+        return res.send({
+          type: InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE,
+          data: {
+            content: text,
+          }
+        });
+      }
+
+      const LOADING_MESSAGE_URL = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/@original`;
+
+      async function replaceLoadingMessage(text) {
+        return DiscordRequest(LOADING_MESSAGE_URL, {
+          method: "PATCH",
+          body: {
+            content: text,
+          },
+        });
+      }
+
+      async function sendResultMessage(messageBody) {
+        return DiscordRequest(`webhooks/${process.env.APP_ID}/${req.body.token}`, {
+          method: "POST",
+          body: messageBody,
+        });
+      }
+
+      // /minifig <id>
+      // Look up a minifig by its Rebrickable ID
+      if (name === "minifig") {
+        let minifigID = req.body.data.options[0].value;
+
+        // The format for these names is "fig-000657", so if we just got a number, make it match that
+        if (/^\d+$/.test(minifigID)) {
+          minifigID = "fig-" + '0'.repeat(Math.max(6 - minifigID.length, 0)) + minifigID;
         }
-      });
-    }
 
-    const LOADING_MESSAGE_URL = `webhooks/${process.env.APP_ID}/${req.body.token}/messages/@original`;
+        await sendLoadingMessage(`Loading minifig #${minifigID}...`);
 
-    async function replaceLoadingMessage(text) {
-      return DiscordRequest(LOADING_MESSAGE_URL, {
-        method: "PATCH",
-        body: {
-          content: text,
-        },
-      });
-    }
+        // Fetch data from Rebrickable
+        let minifigJSON;
+        try {
+          minifigJSON = await getJSON(`https://rebrickable.com/api/v3/lego/minifigs/${minifigID}/?key=${process.env.REBRICKABLE_KEY}`);
+        } catch (error) {
+          console.error(`Rebrickable API request failed: ${error.message}`);
+          return replaceLoadingMessage(`:warning: Rebrickable API request failed: ${error.message}`);
+        }
 
-    async function sendResultMessage(messageBody) {
-      return DiscordRequest(`webhooks/${process.env.APP_ID}/${req.body.token}`, {
-        method: "POST",
-        body: messageBody,
-      });
-    }
+        // If Rebrickable gave us an error, show it instead.
+        if (minifigJSON.detail) {
+          return replaceLoadingMessage(`:warning: Unable to get minifig '${minifigID}': ${minifigJSON.detail}`);
+        }
 
-    // /minifig <id>
-    // Look up a minifig by its Rebrickable ID
-    if (name === "minifig") {
-      let minifigID = req.body.data.options[0].value;
-
-      // The format for these names is "fig-000657", so if we just got a number, make it match that
-      if (/^\d+$/.test(minifigID)) {
-        minifigID = "fig-" + '0'.repeat(Math.max(6 - minifigID.length, 0)) + minifigID;
-      }
-
-      await sendLoadingMessage(`Loading minifig #${minifigID}...`);
-
-      // Fetch data from Rebrickable
-      let minifigJSON;
-      try {
-        minifigJSON = await getJSON(`https://rebrickable.com/api/v3/lego/minifigs/${minifigID}/?key=${process.env.REBRICKABLE_KEY}`);
-      } catch (error) {
-        console.error(`Rebrickable API request failed: ${error.message}`);
-        return replaceLoadingMessage(`:warning: Rebrickable API request failed: ${error.message}`);
-      }
-
-      // If Rebrickable gave us an error, show it instead.
-      if (minifigJSON.detail) {
-        return replaceLoadingMessage(`:warning: Unable to get minifig '${minifigID}': ${minifigJSON.detail}`);
-      }
-
-      // Send completed message
-      return sendResultMessage({
-        embeds: [{
-          title: `${minifigJSON.name}`,
-          description: `
+        // Send completed message
+        return sendResultMessage({
+          embeds: [{
+            title: `${minifigJSON.name}`,
+            description: `
 ${minifigJSON.num_parts} parts
 [View on Rebrickable](<${minifigJSON.set_url}>)`,
-          image: {
-            url: minifigJSON.set_img_url,
-          },
-        }],
-      });
-    }
-
-    // /part <id>
-    // Look up a part by its Lego ID
-    if (name === "part") {
-      // Get the id
-      const partID = req.body.data.options[0].value;
-
-      await sendLoadingMessage(`Loading part #${partID}...`);
-
-      // Fetch data from Rebrickable
-      let partJSON;
-      try {
-        partJSON = await getJSON(`https://rebrickable.com/api/v3/lego/parts/${partID}/?key=${process.env.REBRICKABLE_KEY}`);
-      } catch (error) {
-        console.error(`Rebrickable API request failed: ${error.message}`);
-        return replaceLoadingMessage(`:warning: Rebrickable API request failed: ${error.message}`);
+            image: {
+              url: minifigJSON.set_img_url,
+            },
+          }],
+        });
       }
 
-      // If Rebrickable gave us an error, show it instead.
-      if (partJSON.detail) {
-        return replaceLoadingMessage(`:warning: Unable to get part '${partID}': ${partJSON.detail}`);
-      }
+      // /part <id>
+      // Look up a part by its Lego ID
+      if (name === "part") {
+        // Get the id
+        const partID = req.body.data.options[0].value;
 
-      // Send completed message
-      return sendResultMessage({
-        embeds: [{
-          title: `Part ${partJSON.part_num}: ${partJSON.name}`,
-          description: `
+        await sendLoadingMessage(`Loading part #${partID}...`);
+
+        // Fetch data from Rebrickable
+        let partJSON;
+        try {
+          partJSON = await getJSON(`https://rebrickable.com/api/v3/lego/parts/${partID}/?key=${process.env.REBRICKABLE_KEY}`);
+        } catch (error) {
+          console.error(`Rebrickable API request failed: ${error.message}`);
+          return replaceLoadingMessage(`:warning: Rebrickable API request failed: ${error.message}`);
+        }
+
+        // If Rebrickable gave us an error, show it instead.
+        if (partJSON.detail) {
+          return replaceLoadingMessage(`:warning: Unable to get part '${partID}': ${partJSON.detail}`);
+        }
+
+        // Send completed message
+        return sendResultMessage({
+          embeds: [{
+            title: `Part ${partJSON.part_num}: ${partJSON.name}`,
+            description: `
 - [BrickLink](<https://www.bricklink.com/v2/catalog/catalogitem.page?P=${partJSON.external_ids['BrickLink']}>)
 - [Rebrickable](<${partJSON.part_url}>)`,
-          image: {
-            url: partJSON.part_img_url
-          }
-        }]
-      });
-    }
-
-    // /set <id>
-    // Look up a set by its Lego ID
-    if (name === "set") {
-      // Get the id
-      let setID = req.body.data.options[0].value;
-
-      // Rebrickable set IDs always have a dash and a number at the end. If we don't have one, add `-1`.
-      if (!setID.includes('-')) {
-        setID += "-1";
+            image: {
+              url: partJSON.part_img_url
+            }
+          }]
+        });
       }
 
-      await sendLoadingMessage(`Loading set #${setID}...`);
+      // /set <id>
+      // Look up a set by its Lego ID
+      if (name === "set") {
+        // Get the id
+        let setID = req.body.data.options[0].value;
 
-      // Fetch data from Rebrickable
-      let setJSON;
-      try {
-        setJSON = await getJSON(`https://rebrickable.com/api/v3/lego/sets/${setID}/?key=${process.env.REBRICKABLE_KEY}`);
-      } catch (error) {
-        console.error(`Rebrickable API request failed: ${error.message}`);
-        return replaceLoadingMessage(`:warning: Rebrickable API request failed: ${error.message}`);
-      }
+        // Rebrickable set IDs always have a dash and a number at the end. If we don't have one, add `-1`.
+        if (!setID.includes('-')) {
+          setID += "-1";
+        }
 
-      // If Rebrickable gave us an error, show it instead.
-      if (setJSON.detail) {
-        return replaceLoadingMessage(`:warning: Unable to get set '${setID}': ${setJSON.detail}`);
-      }
+        await sendLoadingMessage(`Loading set #${setID}...`);
 
-      // Send completed message
-      return sendResultMessage({
-        embeds: [{
-          title: `${setJSON.name} (${setJSON.set_num})`,
-          description: `
+        // Fetch data from Rebrickable
+        let setJSON;
+        try {
+          setJSON = await getJSON(`https://rebrickable.com/api/v3/lego/sets/${setID}/?key=${process.env.REBRICKABLE_KEY}`);
+        } catch (error) {
+          console.error(`Rebrickable API request failed: ${error.message}`);
+          return replaceLoadingMessage(`:warning: Rebrickable API request failed: ${error.message}`);
+        }
+
+        // If Rebrickable gave us an error, show it instead.
+        if (setJSON.detail) {
+          return replaceLoadingMessage(`:warning: Unable to get set '${setID}': ${setJSON.detail}`);
+        }
+
+        // Send completed message
+        return sendResultMessage({
+          embeds: [{
+            title: `${setJSON.name} (${setJSON.set_num})`,
+            description: `
 Released in ${setJSON.year}
 ${setJSON.num_parts} parts
 [View on Rebrickable](<${setJSON.set_url}>)`,
-          image: {
-            url: setJSON.set_img_url,
-          }
-        }]
-      });
+            image: {
+              url: setJSON.set_img_url,
+            }
+          }]
+        });
+      }
+
+      console.error(`unknown command: ${name}`);
+      return res.status(400).json({ error: 'unknown command' });
     }
 
-    console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
+    console.error('unknown interaction type', type);
+    return res.status(400).json({ error: 'unknown interaction type' });
+  } catch (err) {
+    console.error("Unhandled exception!", err);
+    try {
+      return res.sendStatus(500);
+    } catch (_) {
+    }
   }
-
-  console.error('unknown interaction type', type);
-  return res.status(400).json({ error: 'unknown interaction type' });
 });
 
 app.listen(PORT, () => {
