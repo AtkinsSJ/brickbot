129a578176f328f60cbb129cd19a6eafcabc4bad
Sam Atkins <atkinssj@gmail.com>
2025-05-05 13:36:09 +0100

    Define magic numbers for command definitions

--- commands.js
+++ commands.js
@@ -1,19 +1,42 @@
 import 'dotenv/config';
 import {InstallGlobalCommands} from './utils.js';
 
+// https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-types
+const COMMAND_TYPE = {
+  CHAT_INPUT: 1,
+  USER: 2,
+  MESSAGE: 3,
+  PRIMARY_ENTRY_POINT: 4,
+};
+
+// https://discord.com/developers/docs/interactions/application-commands#application-command-object-application-command-option-type
+const OPTION_TYPE = {
+  SUB_COMMAND: 1,
+  SUB_COMMAND_GROUP: 2,
+  STRING: 3,
+  INTEGER: 4,
+  BOOLEAN: 5,
+  USER: 6,
+  CHANNEL: 7,
+  ROLE: 8,
+  MENTIONABLE: 9,
+  NUMBER: 10,
+  ATTACHMENT: 11,
+};
+
 const MINIFIG_COMMAND = {
   name: "minifig",
   description: "Look up a minifig",
   options: [
     {
-      type: 3,
+      type: OPTION_TYPE.STRING,
       name: "id",
       description: "Rebrickable number (`fig-001234` or just `1234`)",
       required: true,
       max_length: 50,
     }
   ],
-  type: 1,
+  type: COMMAND_TYPE.CHAT_INPUT,
 };
 
 const PART_COMMAND = {
@@ -21,14 +44,14 @@ const PART_COMMAND = {
   description: "Look up a part",
   options: [
     {
-      type: 3,
+      type: OPTION_TYPE.STRING,
       name: "id",
       description: "Lego part ID",
       required: true,
       max_length: 50,
     }
   ],
-  type: 1,
+  type: COMMAND_TYPE.CHAT_INPUT,
 };
 
 const SET_COMMAND = {
@@ -36,14 +59,14 @@ const SET_COMMAND = {
   description: "Look up a set",
   options: [
     {
-      type: 3,
+      type: OPTION_TYPE.STRING,
       name: "id",
       description: "Lego set ID",
       required: true,
       max_length: 50,
     }
   ],
-  type: 1,
+  type: COMMAND_TYPE.CHAT_INPUT,
 };
 
 const ALL_COMMANDS = [MINIFIG_COMMAND, PART_COMMAND, SET_COMMAND];
