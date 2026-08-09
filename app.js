d1d76f13b9e696a309c5f2ad94e1327f49c9ef84
Sam Atkins <atkinssj@gmail.com>
2026-08-09 10:31:51 +0100

    Remove HTTPS handling
    This isn't something we should handle internally, but instead, BrickBot
    serves standard HTTP, and the reverse proxy deals with HTTPS on the
    outside.

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import 'dotenv/config';
 import express from 'express';
 import {InteractionResponseType, InteractionType, verifyKeyMiddleware,} from 'discord-interactions';
-import * as fs from "node:fs";
-import * as https from "node:https";
 import {ThemeManager} from "./src/ThemeManager.js";
 import {Commands} from "./src/Commands.js";
-import * as path from "node:path";
 
 process.title = "BrickBot";
 
@@ -63,42 +60,6 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
   }
 });
 
-function readCertificates() {
-  return {
-    cert: fs.readFileSync(process.env.TLS_FULLCHAIN_PATH),
-    key: fs.readFileSync(process.env.TLS_PRIVKEY_PATH)
-  };
-}
-
-let initialOptions;
-try {
-  initialOptions = readCertificates();
-} catch (err) {
-  initialOptions = undefined;
-  console.error("Failed to read TLS file:", err);
-  console.log("To enable TLS, please set TLS_FULLCHAIN_PATH and TLS_PRIVKEY_PATH in .env");
-  app.listen(PORT, () => {
-    console.log('Listening on port', PORT);
-  });
-}
-
-if (initialOptions !== undefined) {
-  let server = https.createServer(initialOptions, app).listen(PORT, () => {
-    console.log('Listening on port', PORT);
-  });
-
-// https://stackoverflow.com/a/74076392/1178345
-// Refresh httpd's certs when certs change on disk. The timeout stuff
-// "ensures" that all 3 relevant files are updated, and accounts for
-// sometimes trigger-happy fs.watch.
-  let certificateWatchPath = path.dirname(fs.realpathSync(process.env.TLS_PRIVKEY_PATH));
-  let waitForCertAndFullChainToGetUpdatedTooTimeout;
-  console.log(`Watching ${certificateWatchPath}...`);
-  fs.watch(certificateWatchPath, () => {
-    clearTimeout(waitForCertAndFullChainToGetUpdatedTooTimeout);
-    waitForCertAndFullChainToGetUpdatedTooTimeout = setTimeout(() => {
-      server.setSecureContext(readCertificates());
-      console.log("Reloaded SSL certificates");
-    }, 1000);
-  });
-}
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+});
