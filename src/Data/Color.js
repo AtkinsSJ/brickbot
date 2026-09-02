518e23f563eafcf49f80265e51c74218a1148892
Sam Atkins <atkinssj@gmail.com>
2026-09-02 15:08:31 +0100

    Store Color ids and names as Maps
    Again, this is the natural type for these, instead of using objects.

--- src/Data/Color.js
+++ src/Data/Color.js
@@ -1,48 +1,45 @@
 import {generateInfoBox} from "../Discord.js";
-import {sortObjectByKeys} from "../../utils.js";
 import {ImageGenerator} from "../ImageGenerator.js";
 
 export class Color {
   rgb;
   isTransparent;
-  ids = {};
-  names = {};
+  ids = new Map();
+  names = new Map();
 
   static fromRebrickableJSON(json) {
     let color = new Color();
     color.rgb = json.rgb;
     color.isTransparent = json.is_trans;
-    color.ids["Rebrickable"] = json.id;
-    color.names["Rebrickable"] = json.name;
+    color.ids.set("Rebrickable", json.id);
+    color.names.set("Rebrickable", json.name);
 
     // All other names are read from the external_ids data.
     // NB: This produces multiple names, and multiple IDs. We just worry about the first one for now.
     for (let [siteName, data] of Object.entries(json.external_ids)) {
       const firstID = data.ext_ids[0];
       if (Array.isArray(firstID)) {
-        color.ids[siteName] = firstID[0];
+        color.ids.set(siteName, firstID[0]);
       } else {
-        color.ids[siteName] = firstID;
+        color.ids.set(siteName, firstID);
       }
       const firstDescriptor = data.ext_descrs[0];
       if (Array.isArray(firstDescriptor)) {
-        color.names[siteName] = firstDescriptor[0];
+        color.names.set(siteName, firstDescriptor[0]);
       } else {
-        color.names[siteName] = firstDescriptor;
+        color.names.set(siteName, firstDescriptor);
       }
     }
-
-    color.names = sortObjectByKeys(color.names);
 
     return color;
   }
 
   get id() {
-    return this.ids["Rebrickable"];
+    return this.ids.get("Rebrickable");
   }
 
   get name() {
-    return this.names["Rebrickable"];
+    return this.names.get("Rebrickable");
   }
 
   async getDiscordMessageJSON(request) {
@@ -50,13 +47,15 @@ export class Color {
     const png = await ImageGenerator.instance.generatePNGColorSwatch(this.rgb, this.isTransparent);
 
     let description = `
-## Color ${this.names["Rebrickable"]}    
+## Color ${this.names.get("Rebrickable")}    
 `;
 
-    for (let [siteName, name] of Object.entries(this.names)) {
-      const id = this.ids[siteName];
+    const sortedSiteNames = this.names.keys().toArray().sort();
+    for (const siteName of sortedSiteNames) {
+      const id = this.ids.get(siteName);
+      const name = this.names.get(siteName);
       if (id) {
-        description += `- ${siteName}: ${name} (ID ${this.ids[siteName]})\n`;
+        description += `- ${siteName}: ${name} (ID ${id})\n`;
       } else {
         description += `- ${siteName}: ${name}\n`;
       }
